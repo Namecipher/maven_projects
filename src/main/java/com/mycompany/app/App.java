9e55ee261e9437d43d74a15be3faccc7916dcce0
Namecipher <jhaabhishek115@gmail.com>
2022-08-11 23:08:39 +0530

    App file updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Github Webhook!" );
+        System.out.println( "Hello EC2 Jenkins Job" );
     }
 }
